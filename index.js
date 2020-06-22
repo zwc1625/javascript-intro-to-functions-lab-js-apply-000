e48c3725a7c0ab3be5d85fa18fc76d5e0c64ab46
Weichang Zheng <weichang1625@gmail.com>
2020-06-22 19:06:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 function shout (string) {
   return string.toUpperCase(); 
 }
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if(string====string.toLowerCase()) {
+  }
+  return "I can't hear you!"
+}
